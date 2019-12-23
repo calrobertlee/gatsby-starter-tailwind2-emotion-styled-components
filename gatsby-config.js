e6d6942140f3778d097ce069ffa8908e0610e646
n0k5 <hey@chrish.design>
2019-12-23 19:54:02 +0000

    🧪 Init

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Tailwind 2 + Styled-Components`,
+    description: `A (reasonably) unopinionated Gatsby starter, including; Tailwind 2`,
+    author: `@chrish-d`
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-resolve-src`
+  ]
+};
