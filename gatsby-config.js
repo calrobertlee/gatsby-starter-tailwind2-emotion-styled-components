5dfcff67ca75affefd711d855397f83f85e584f5
n0k5 <hey@chrish.design>
2019-12-23 22:33:09 +0000

    [+] Add Emotion and tailwind.macro and config both

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Tailwind 2 + Styled-Components`,
+    title: `Gatsby Tailwind 2 + Emotion / Styled Components`,
     description: `A (reasonably) unopinionated Gatsby starter, including; Tailwind 2`,
-    author: `@chrish-d`
+    author: `@chrish-d`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-resolve-src`
-  ]
-};
+    `gatsby-plugin-resolve-src`,
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {},
+    },
+  ],
+}
