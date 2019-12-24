96cf28b9dd782627a44f6cfef5857e518c078cae
n0k5 <hey@chrish.design>
2019-12-24 14:19:37 +0000

    [+] Add missing dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     author: `@chrish-d`,
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-resolve-src`,
